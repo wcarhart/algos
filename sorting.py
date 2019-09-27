547a824f4437a4d96741a56392013a77bf086d4f
Will Carhart <wcarhart@sandiego.edu>
2019-09-27 00:11:31 -0700

    Started on sorting

--- sorting.py
+++ sorting.py
@@ -1,16 +1,95 @@
 from heap import MaxHeap
+from random import randint
+
+def unsort(arr):
+	"""
+	We pick a random index in the array. Then, we pull out the value at that
+	index and save it in our result. Finally, we pop the last element in the 
+	array and move it into the random index. We repeat this until our original
+	array is empty and our result is full.
+	"""
+	to_return = []
+	for i in range(0, len(arr)):
+		index = randint(0, len(arr)-1)
+		to_return.append(arr[index])
+		arr[index] = arr[-1]
+		arr.pop()
+	return to_return
 
 # TODO
 def binarysearch(arr, value):
 	return
 
-# TODO
 def bubblesort(arr):
-	return
+	"""
+	We iterate through each item in the array. If the item is bigger than the
+	item directly to the right of it, we swap the two elements. This causes the
+	smallest elements to "bubble" up to the front of the array.
+	"""
+	is_sorted = False
+	while not is_sorted:
+		is_sorted = True
+		for i in range(0, len(arr) - 1):
+			if arr[i] > arr[i+1]:
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				is_sorted = False
 
-# TODO
+def bubblesort_improved(arr):
+	"""
+	Bubble sort iterates through the whole array over and over. With each
+	iteration, we can observe that the greatest element gets slotted into the
+	correct index, until the whole array is sorted. Thus, for an unsorted array
+	of n numbers, we only need to compare the first n-1 numbers; we can shrink
+	the end index by one during each iteration.
+	"""
+	is_sorted = False
+	last_unsorted = len(arr) - 1
+	while not is_sorted:
+		is_sorted = True
+		for i in range(0, last_unsorted):
+			if arr[i] > arr[i+1]:
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				is_sorted = False
+		last_unsorted -= 1
+
+def cocktailshakersort(arr):
+	"""
+	Improving on bubblesort_improved, we can use the same strategy with the
+	start index, so the elements of the array bubble up and down with each
+	iteration, effectively shrinking our unsorted portion on both ends.
+	"""
+	is_sorted = False
+	start_index = 0
+	end_index = len(arr) - 1
+	while not is_sorted:
+		is_sorted = True
+		for i in range(start_index, end_index):
+			if arr[i] > arr[i+1]:
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				is_sorted = False
+		end_index -= 1
+		for i in range(end_index, start_index, -1):
+			if arr[i] < arr[i-1]:
+				arr[i], arr[i-1] = arr[i-1], arr[i]
+				is_sorted = False
+		start_index += 1
+
 def insertionsort(arr):
-	return
+	"""
+	We segment the array into two portions: sorted and unsorted. Initially, the
+	sorted section is just the first element in the array. Then, we take an
+	element from the unsorted section and "insert" it into the sorted section.
+	We do this by removing the element from the array (creating a slot), and
+	then shifting the sorted portion up by one before inserting the element
+	back into its correct index.
+	"""
+	for i in range(1, len(arr)):
+		temp = arr[i]
+		slot = i
+		while slot > 0 and arr[slot-1] > temp:
+			arr[slot] = arr[slot-1]
+			slot -= 1
+		arr[slot] = temp
 
 # TODO
 def mergesort(arr):
@@ -29,9 +108,44 @@ def selectionsort(arr):
 	return
 
 # TODO
+def doubleselectionsort(arr):
+	return
+
+# TODO
 def heapsort(arr):
 	return
 
 # TODO
 def radixsort(arr):
 	return
+
+# TODO
+def bogosort(arr):
+	return
+
+if __name__ == '__main__':
+	# arr = [1,2,3,4,5]
+	# print(unsort(arr))
+	# print('bubble sort')
+	arr = [3, 2, 5, 1, 4, 6, 8, 10, 0, 9, 7]
+	# bubblesort(arr)
+	# print(arr)
+
+	# print('bubble sort improved')
+	# arr = [3, 2, 5, 1, 4, 6, 8, 10, 0, 9, 7]
+	# bubblesort_improved(arr)
+	# print(arr)
+	# cocktailshakersort(arr)
+	insertionsort(arr)
+	print(arr)
+
+
+
+
+
+
+
+
+
+
+
